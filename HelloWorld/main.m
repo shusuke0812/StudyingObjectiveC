63270552318602be42ea8a3efe762014ac81f56c
shusuke0812 <shu.ota0812@gmail.com>
2020-08-19 17:33:34 +0900

    [add]実行ファイル追加

---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+#import "Song.h"
+#import "Singer.h"
+
+// 実行プログラム
+int main(void) {
+	id song;
+	id singer;
+
+	song = [[Song alloc] init];
+	[song setLyrics:@"La La La ..."];
+
+	singer = [Singer alloc] init];
+  [singer setSong:song];
+	[singer sing];
+
+	return 0;
+}
